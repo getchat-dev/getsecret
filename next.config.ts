44f18d70e15c977328c8246b32e562440ec1b859
Evgeniy Tyurin <et@markuper.com>
2026-05-14 01:09:33 +0300

    feat(config): allow dev origin at burnotes.local

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,7 @@ const nextConfig: NextConfig = {
     output: 'standalone',
     poweredByHeader: false,
     serverExternalPackages: ['ioredis'],
+    allowedDevOrigins: ['http://burnotes.local'],
     async headers() {
         return [
             {
